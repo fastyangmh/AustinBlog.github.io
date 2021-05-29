f44430e8f2cd6fc9db076712dd188f4ff566531d
YangMH <fastyangmh@gmail.com>
2021-05-29 19:32:34 +0800

    change the locale value from zh-TW to en-US. add the Chinese version of the introduction.

--- _posts/2021-05-29-About_this_blog.md
+++ _posts/2021-05-29-About_this_blog.md
@@ -2,13 +2,13 @@
 title: About this blog
 layout: single
 category: 
-tag:
+tag: Introduction
 ---
 I am Austin, with a master’s degree in Computer Science and Information Engineer from the National Yunlin University of Science and Technology.  
 
 I am familiar with image classification and sound recognition, and I am good at PyTorch, PyTorch Lightning, and TensorFlow in the deep learning framework.  
 
-In this blog, I will introduce deep learning, leetcode problem, or some programming problems, and keep the notes as my programming diary.  
+In this blog, I will introduce deep learning, LeetCode problem, or some programming problems, and keep the notes as my programming diary.  
 
 If you have any questions, please feel free to contact me by email. 
 
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
         "title": "About this blog",
-        "excerpt":"I am Austin, with a master’s degree in Computer Science and Information Engineer from the National Yunlin University of Science and Technology.   I am familiar with image classification and sound recognition, and I am good at PyTorch, PyTorch Lightning, and TensorFlow in the deep learning framework.   In this blog, I will introduce deep learning, leetcode problem, or some programming problems, and keep the notes as my programming diary.   If you have any questions, please feel free to contact me by email.   Thank you!  ","categories": [],
-        "tags": [],
+        "excerpt":"I am Austin, with a master’s degree in Computer Science and Information Engineer from the National Yunlin University of Science and Technology.   I am familiar with image classification and sound recognition, and I am good at PyTorch, PyTorch Lightning, and TensorFlow in the deep learning framework.   In this blog, I will introduce deep learning, LeetCode problem, or some programming problems, and keep the notes as my programming diary.   If you have any questions, please feel free to contact me by email.   Thank you!  ","categories": [],
+        "tags": ["Introduction"],
         "url": "/About_this_blog/",
+        "teaser": null
+      },{
+        "title": "關於這個部落格",
+        "excerpt":"我叫Austin，畢業於國立雲林科技大學資訊工程系碩士。   熟悉影像分類和聲音識別，擅長深度學習框架中的PyTorch、PyTorch Lightning、TensorFlow。   在這部落格中，我將介紹深度學習、LeetCode問題，或者一些程式問題，並放上我的程式日記。   如果您有任何問題，請隨時透過電子郵件與我聯繫。   謝謝！  ","categories": [],
+        "tags": ["介紹"],
+        "url": "/%E9%97%9C%E6%96%BC%E9%80%99%E5%80%8B%E9%83%A8%E8%90%BD%E6%A0%BC/",
         "teaser": null
       }]
